90cf63a7521ff27ddf17e1ece31b390136641365
Estela <estela.valiente@gmail.com>
2024-09-21 12:45:44 +0100

    secont iteration

--- index.js
+++ index.js
@@ -22,9 +22,10 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const camelTail = part1.slice(0, -1) + part1.slice(-1).toUpperCase() + part2.slice(0, -1) + part2.slice(-1).toUpperCase();
 
 // Print the cameLtaiL-formatted string
+console.log(camelTail);
 
 
 
